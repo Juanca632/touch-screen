1a3f3e1aab78b562accf5cb460e1530e835b7cff
juanca632 <juanca632@hotmail.com>
2023-12-12 16:03:36 +0100

    relay added

--- script.js
+++ script.js
@@ -38,31 +38,55 @@ function onConnectionLost(responseObject){
 function onMessageArrived(message){
     // console.log("OnMessageArrived: "+message.payloadString);
 
-    var payload = message.payloadString;
+    let payload = message.payloadString;
 
     if(message.destinationName === "battery/voltage"){
+        setTimeout(function () {
             document.getElementById("voltage").innerHTML = payload;
+        }, 500);
     }
     else if(message.destinationName === "battery/SOC"){
+        setTimeout(function () {
             document.getElementById("soc").innerHTML = payload;
+        }, 500);
     }
     else if(message.destinationName === "battery/SOH"){
+        setTimeout(function () {
             document.getElementById("soh").innerHTML = payload;
+        }, 500);
     }
     else if(message.destinationName === "battery/power"){
+        setTimeout(function () {
             document.getElementById("power").innerHTML = payload;
+        }, 500);
     }
     else if(message.destinationName === "battery/capacity"){
+        setTimeout(function () {
             document.getElementById("capacity").innerHTML = payload;
+        }, 500);
     }
     else if(message.destinationName === "battery/current"){
+        setTimeout(function () {
             document.getElementById("current").innerHTML = payload;
+        }, 500);
     }
     else if(message.destinationName === "battery/temperature"){
+        setTimeout(function () {
             document.getElementById("temperature").innerHTML = payload;
+        }, 500);
     }
     else if(message.destinationName === "relay"){
-            document.getElementById("flexSwitchCheckDefault").innerHTML = payload;
+        checkbox = document.getElementById("flexSwitchCheckDefault");
+        console.log(payload);
+
+        if(payload == "1"){
+            checkbox.checked = true;
+        }
+        else{
+            checkbox.checked = false;
+        }
+        
+
     }
 
 }
@@ -83,20 +107,17 @@ let button_relay = 0
 
 function publishMessage(){
     topic = "relay";
-    // if(button_relay == 0){
-    //     button_relay = 1;
-    //     msg = 1;
-    // }
-    // else{
-    //     button_relay = 0;
-    //     msg = 0;
-    // }
-    console.log("hola");
-    msg = "On";
-
+    checkbox = document.getElementById("flexSwitchCheckDefault");
+    // console.log(checkbox.checked);
+    if(checkbox.checked == false){
+        msg = "0";
+    }
+    else{
+        msg = "1";
+    }
     Message = new Paho.MQTT.Message(msg);
     Message.destinationName = topic;
-
+    Message.retained = true;
     client.send(Message);
 }
 
